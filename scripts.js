45f29b39e9b0a20bb18de9e2b37889febacd5a66
Sova <108209320+NceSova@users.noreply.github.com>
2024-06-05 14:02:41 +0300

    add color picker and dimensions selector

--- scripts.js
+++ scripts.js
@@ -1,18 +1,48 @@
-const canvas = document.querySelector("#canvas")
+const canvas = document.querySelector('#canvas')
+const clearButton = document.querySelector('#clear')
+
 let num = 16;
-let dimensions = 400/num;
-for (let x = 0; x < num; x++) {
-  let row = document.createElement('div')
-  row.style.cssText = 'flex:'
-  for (let y = 0; y < num; y++) {
-    let newDiv = document.createElement('div')
-    newDiv.style.cssText = `background-color: white; width: ${dimensions}px; height:${dimensions}px; flex: 1 1 1`
-    row.appendChild(newDiv)
-    console.log('div created')
+let dimensions = 400 / num;
+
+function generateCanvas(numOfPixels) {
+  for (let x = 0; x < numOfPixels; x++) {
+    let row = document.createElement('div')
+    row.style.cssText = 'flex:'
+    for (let y = 0; y < numOfPixels; y++) {
+      let newDiv = document.createElement('div')
+      newDiv.style.cssText = `background-color: white; width: ${dimensions}px; height:${dimensions}px; flex: 1 1 1`
+      row.appendChild(newDiv)
+      console.log('div created')
+    }
+    canvas.appendChild(row)
   }
-  canvas.appendChild(row)
 }
 
-canvas.addEventListener("mouseover", (e) => {
-  e.target.style.backgroundColor = 'blue'
+generateCanvas(num);
+const pixels = document.querySelectorAll('#canvas div')
+const colorInput = document.querySelector('#color')
+const numInput = document.querySelector('#number')
+
+numInput.addEventListener('change', (e) => {
+  num = e.target.value
+  dimensions = 400 / num;
+  canvas.innerHTML = ''
+  generateCanvas(num)
+})
+
+
+let color = '#0000ff'
+colorInput.addEventListener('change', (e) => {
+  color = e.target.value
+})
+
+canvas.addEventListener("click", (e) => {
+  e.preventDefault()
+  e.target.style.backgroundColor = `${color}`
+})
+
+clearButton.addEventListener('click', () => {
+  pixels.forEach((pixel) => {
+    pixel.style.backgroundColor = 'white'
+  })
 })
