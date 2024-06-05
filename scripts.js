a38a585f20b3ac6aca9424a6eccdb336c14335a3
Sova <108209320+NceSova@users.noreply.github.com>
2024-06-05 10:00:33 +0300

    add basic paint functionality

--- scripts.js
+++ scripts.js
@@ -1,12 +1,18 @@
 const canvas = document.querySelector("#canvas")
 let num = 16;
+let dimensions = 400/num;
 for (let x = 0; x < num; x++) {
   let row = document.createElement('div')
+  row.style.cssText = 'flex:'
   for (let y = 0; y < num; y++) {
     let newDiv = document.createElement('div')
-    newDiv.style.cssText = 'background-color: red; width: 20px; height:20px; border: 2px solid blue;'
+    newDiv.style.cssText = `background-color: white; width: ${dimensions}px; height:${dimensions}px; flex: 1 1 1`
     row.appendChild(newDiv)
     console.log('div created')
   }
   canvas.appendChild(row)
 }
+
+canvas.addEventListener("mouseover", (e) => {
+  e.target.style.backgroundColor = 'blue'
+})
